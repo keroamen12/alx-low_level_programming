50bd8ac4338db1499cae8314cfddd8a5e23e0eed
keroamen12 <kerolos.amen@ejust.edu.eg>
2023-03-16 15:16:46 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (Success)
- */
-=======
->>>>>>> b4cb6ece9f50c5255275e4b7860bf7aa91d3285a
-int main(void) 
+* main - Entry point
+*
+* Return: Alawys 0 (Success)
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
